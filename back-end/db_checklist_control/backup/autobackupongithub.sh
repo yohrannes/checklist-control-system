14aea63edcee51fae55cb0bfe93261066cc88183
yohrannes <yohrannes_tribalwars@hotmail.com>
2023-07-09 12:34:01 +0000

    Automated backup

--- back-end/db_checklist_control/backup/autobackupongithub.sh
+++ back-end/db_checklist_control/backup/autobackupongithub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f /var/lib/jelastic/backup/db_checklistcontrol*
 /var/lib/jelastic/bin/backup_script.sh -m dump -c 15 -u root -p FLCkbq45934 -d db_checklistcontrol
-cp /var/lib/jelastic/backup/db_checklistcontrol* /root/checklist-control-system/back-end/db_checklist_control/backup
+cp /var/lib/jelastic/backup/db_checklistcontrol* /root/checklist-control-system/back-end/db_checklist_control/backup/db_checklistcontrol
 cd /root/checklist-control-system/back-end/db_checklist_control/backup
 git add --all 
 git commit -am "Automated backup"
